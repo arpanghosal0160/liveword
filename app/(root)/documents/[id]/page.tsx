08a9ab93b5beabf105c11d72ce284816a7ee5fdd
Arpan Ghosal <arpanghosal0160@gmail.com>
2024-07-30 22:47:22 +0530

    commit

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { Editor } from '@/components/ui/Editor'
+import React from 'react'
+
+const document = () => {
+  return (
+    <div>
+        <Editor />
+    </div>
+  )
+}
+
+export default document
